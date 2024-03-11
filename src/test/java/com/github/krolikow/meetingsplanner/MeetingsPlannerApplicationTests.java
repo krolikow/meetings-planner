1c0cf30fd5a7bfe02a0bb2a4051b4a6b0c03be5f
Magdalena <mkrolikowska01@poczta.fm>
2024-03-11 12:27:13 +0100

    Initial commit

---
+++ src/test/java/com/github/krolikow/meetingsplanner/MeetingsPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.krolikow.meetingsplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
