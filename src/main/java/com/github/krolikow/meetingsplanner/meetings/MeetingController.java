4f444371bc1914f9f6cf970d857a90a0f136590a
Magdalena <mkrolikowska01@poczta.fm>
2024-03-25 23:56:33 +0100

    fix: fix path mappings

--- src/main/java/com/github/krolikow/meetingsplanner/meetings/MeetingController.java
+++ src/main/java/com/github/krolikow/meetingsplanner/meetings/MeetingController.java
@@ -2,13 +2,12 @@ package com.github.krolikow.meetingsplanner.meetings;
 
 import com.github.krolikow.meetingsplanner.common.exceptions.MeetingNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/meetings")
+@RestController
+@RequestMapping("api/meetings")
 public class MeetingController {
     private final MeetingService meetingService;
 
@@ -17,7 +16,7 @@ public class MeetingController {
         this.meetingService = meetingService;
     }
 
-    @GetMapping("/")
+    @GetMapping("")
     public List<Meeting> findAll() {
         return meetingService.findAll();
     }
@@ -31,13 +30,13 @@ public class MeetingController {
         return meeting;
     }
 
-    @PostMapping("/")
+    @PostMapping("")
     public Meeting addMeeting(@RequestBody Meeting meeting) {
         meeting.setId(0);
         return meetingService.save(meeting);
     }
 
-    @PutMapping("/")
+    @PutMapping("")
     public Meeting updateMeeting(@RequestBody Meeting meeting) {
         return meetingService.save(meeting);
     }
